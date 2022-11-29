d689582b03d3aae3fb72446baead39ec44aa26f8
Bruno Becoski <brunobecoski@gmail.com>
2022-11-28 23:25:57 -0300

    chore: update component TextFieldCurrency

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, MutableRefObject, useRef } from 'react';
+import { ChangeEvent, InputHTMLAttributes, MutableRefObject, useRef, useState } from 'react';
 
 import { TextFieldContainer, Border, Label, Input, Span } from './styles';
 
@@ -33,8 +33,48 @@ export function TextField({ label, name, errorMessage, ...props }: TextFieldProp
 }
 
 export function TextFieldCurrency({ label, name, errorMessage, ...props }: TextFieldProps) { 
+  const [price, setPrice] = useState('');
+
   const inputRef = useRef() as  MutableRefObject<HTMLInputElement>
   
+  function formatPrice(event: ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+
+    const valueInt = value
+      .replace(/[R$,\.a-zA-z]+/g, '')
+      .replace(/\b0+/g, '')
+      .trim()
+
+    let valueFormatted = ''
+
+    const valueIntLenght = valueInt.length
+
+    switch (valueIntLenght) {
+      case 0:
+        valueFormatted = ''
+        break;
+      case 1:
+        console.log('um')
+        valueFormatted = valueInt
+        break;
+      
+      case 2:
+        console.log('dois')
+        valueFormatted = valueInt
+        break;
+        
+      default:
+        valueFormatted = `${
+          valueInt.substring(0, valueIntLenght-2).replace(/\d(?=(?:\d{3})+$)/g, '$&.')
+          + ',' +
+          valueInt.substring(valueIntLenght-2)
+        }`
+        break;
+      } 
+
+    setPrice(`R$ ${valueFormatted}`)
+  }
+
   return (
     <TextFieldContainer>
       <Border onClick={() => inputRef.current.focus()}>
@@ -46,8 +86,11 @@ export function TextFieldCurrency({ label, name, errorMessage, ...props }: TextF
           ref={inputRef}
           id={name}
           placeholder=" "
+          inputMode="numeric"
           {...props}
           data-invalid={!!errorMessage}
+          onChange={formatPrice}
+          value={price}
         />
       </Border>
       <Span>{errorMessage}</Span> 
